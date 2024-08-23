cbfe3c2b606cdd1a3cfd7d60d9382dd013d08f86
Afaf24 <afafelbadawy1818@gmail.com>
2024-08-23 21:53:21 +0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        productName: 'calculus Course',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'C++ Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'physics course',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+
+    {
+        productName: 'linear algabra course',
+        productNumber: '46452',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+
+    {
+        productName: 'English course',
+        productNumber: '56452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+    
+
+    
+]
